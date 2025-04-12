ecd78bdd15e45664397eedb462cae9ff543690e2
Watermelon16 <phuonglh43@gmail.com>
2025-04-13 00:23:33 +0700

    Thêm mã nguồn chính: app.py, pinns_optimizer.py, database.py

--- pinns_optimizer.py
+++ pinns_optimizer.py
@@ -276,8 +276,8 @@ def create_force_diagram_plotly(H, n, m, xi):
     add_arrow(mid - lG1, H / 3, 0, -5, 'G1',result['G1'])
     add_arrow(mid - lG2, H * (1 - xi) / 3, 0, -5, 'G2', result['G2'])
     add_arrow(mid - lt, 0, 0, -5, 'Wt',result['Wt'])
-    add_arrow(mid - l2, H * (1 - xi) + xi * H / 2, 0, -5, "W'2", result["W'2"])
-    add_arrow(mid - l22, 2/3 * H * (1 - xi), 0, -5, 'W"2', result['W"2'])
+    add_arrow(mid - l2, H * (1 - xi) + xi * H / 2, 0, -5, "W'2", result['W2_1'])
+    add_arrow(mid - l22, 2/3 * H * (1 - xi), 0, -5, 'W"2', result['W2_2'])
     add_arrow(x0 - 2, l1, -5, 0, 'W1', result['W1'])
 
     
