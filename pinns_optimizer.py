115507e9ef2bf111138dd616b50fb6f9d558b304
Watermelon16 <phuonglh43@gmail.com>
2025-04-12 18:46:13 +0700

    Thêm mã nguồn chính: app.py, pinns_optimizer.py, database.py

--- pinns_optimizer.py
+++ pinns_optimizer.py
@@ -440,10 +440,10 @@ def create_actual_dam_profile(H_opt, n, m, xi, H_total, B_top):
     fig.add_shape(type="line", x0=x4, y0=-4, x1=x4, y1=-2.0, line=dict(width=1))
 
     # Kích thước Bđ
-    fig.add_annotation(x=(x5 + x6) / 2, y=H_total + 2.5, text=f"Bđ = {B_top:.2f} m", showarrow=False, font=dict(size=14))
+    fig.add_annotation(x=(x5 + x6) / 2, y=H_total + 3.0, text=f"Bđ = {B_top:.2f} m", showarrow=False, font=dict(size=14))
     fig.add_shape(type="line", x0=x5, y0=H_total + 2.0, x1=x6, y1=H_total + 2.0, line=dict(width=1))
-    fig.add_shape(type="line", x0=x5, y0=H_total + 1.0, x1=x5, y1=H_total + 1.0, line=dict(width=1))
-    fig.add_shape(type="line", x0=x6, y0=H_total + 3.0, x1=x6, y1=H_total + 3.0, line=dict(width=1))
+    fig.add_shape(type="line", x0=x5, y0=H_total + 1.0, x1=x5, y1=H_total + 3.0, line=dict(width=1))
+    fig.add_shape(type="line", x0=x6, y0=H_total + 1.0, x1=x6, y1=H_total + 3.0, line=dict(width=1))
 
     # Hệ số n tại đoạn 1–2
     angle_n = math.degrees(math.atan2(y2 - y1, x2 - x1))
